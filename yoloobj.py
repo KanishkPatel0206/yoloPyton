773b965650be6e220a423810eac496ff48c250d3
KanishkPatel0206 <kanishknp02@gmail.com>
2026-01-22 19:40:15 +0530

    Initial commit (Clean)

---
+++ yoloobj.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolo11m.pt")
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLOv11 Object Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
